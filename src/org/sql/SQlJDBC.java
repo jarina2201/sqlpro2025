8f04ddf7e1b2bd7133d8990d9becab6399a37f6b
jari2201 <a.jarina2201@gmail.com>
2025-06-24 11:42:27 +0530

    Jarina Added new Project

---
+++ src/org/sql/SQlJDBC.java
@@ -0,0 +1,48 @@
+package org.sql;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class SQlJDBC {
+public static void main(String[] args) throws ClassNotFoundException, SQLException {
+	
+	
+	//1.Load the driver 
+	Class.forName("oracle.jdbc.OracleDriver");
+	
+	//2.connect to database
+	Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","Hr","jarina");
+	
+	//3.Sql query
+	
+	String query = "select * from courseDetails";
+	
+	//4.Prepare the statement
+	
+	PreparedStatement ps = con.prepareStatement(query);
+	
+	//5.Execute Query
+	
+	ResultSet eq = ps.executeQuery();
+	
+	//6.Iterate the results
+	
+	while (eq.next()) {
+		
+		int c_id = eq.getInt("course_Id");
+		String c_name = eq.getString("course_name");
+		
+		
+		System.out.println(c_id+" ---->"+c_name);
+	}
+	
+	//7.Close the database connection
+	
+	
+	
+}
+}
