0c11487a03f0537b4f9fc4d2bb8b18aa4a610f63
jari2201 <a.jarina2201@gmail.com>
2025-06-25 20:52:48 +0530

    muthuraj added few methods

--- src/org/sql/SQlJDBC.java
+++ src/org/sql/SQlJDBC.java
@@ -54,7 +54,9 @@ public static void main(String[] args) throws ClassNotFoundException, SQLExcepti
 	int b=20;
 	
 	int c=a+b;
-	System.out.println(c);
+	System.out.println("A + B = "+c);
+	
+	
 	
 	
 	
